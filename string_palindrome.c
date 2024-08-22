bd923c7fcaa82cef21759e322f82932952edb645
Shad CT <88387228+shad-ct@users.noreply.github.com>
2024-08-22 20:33:45 +0530

    Update string_palindrome.c

--- string_palindrome.c
+++ string_palindrome.c
@@ -15,3 +15,29 @@ int main(){
 	}
 	
 }
+
+// ANOTHER METHOD
+
+/*
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char name[100] = "malayalam";
+	int i , j,length=0,flag = 1;
+	while(name[length]!='\0'){ // process of strlen()
+		length++;
+	}
+	for(i=0,j=length-1; i<j ; i++,j--){
+		if(name[i]!=name[j]){
+			flag = 0;
+			break;
+		}
+	}
+	if(flag == 1){
+		printf("pal");
+	}
+	else{
+		printf("not");
+	}
+}
+*/
