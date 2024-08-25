1fdb6316df08a643ce8aa2226eaba3582c0fc9ce
Shad CT <88387228+shad-ct@users.noreply.github.com>
2024-08-25 09:37:28 +0530

    Update Crypt.c

--- CProgramming/Crypt.c
+++ CProgramming/Crypt.c
@@ -5,6 +5,7 @@ void encrypt(char *msg, int pass) {
 	for (int i = 0; i < strlen(msg); i++) {
 		msg[i] = (msg[i] + pass) % 256;
 	}
+	printf("\n-----------------------------\n");
 	printf("Encrypted message: ");
 	for (int i = 0; i < strlen(msg); i++) {
 		printf("%c", msg[i]);
@@ -16,6 +17,7 @@ void decrypt(char *msg, int pass) {
 	for (int i = 0; i < strlen(msg); i++) {
 		msg[i] = (msg[i] - pass + 256) % 256;
 	}
+	printf("\n-----------------------------\n");
 	printf("Decrypted message: ");
 	for (int i = 0; i < strlen(msg); i++) {
 		printf("%c", msg[i]);
@@ -26,22 +28,22 @@ void decrypt(char *msg, int pass) {
 int main() {
 	int choice, pass;
 	char msg[100];
-	printf("Enter your choice : \n");
 	printf(" 1. ENCRYPT \n 2. DECRYPT \n");
 	printf("Enter your choice : ");
 	scanf("%d", &choice);
+	printf("\n-----------------------------\n");
 	switch (choice) {
 	case 1:
-		printf("\nEnter the message to Encrypt : ");
+		printf("Enter the message to Encrypt : ");
 		scanf("%s", msg);
-		printf("Enter your password (integer): ");
+		printf("Enter your password (+ve integer): ");
 		scanf("%d", &pass);
 		encrypt(msg, pass);
 		break;
 	case 2:
 		printf("Enter the message to Decrypt : ");
 		scanf("%s", msg);
-		printf("Enter your password (integer): ");
+		printf("Enter your password (+ve integer): ");
 		scanf("%d", &pass);
 		decrypt(msg, pass);
 		break;
