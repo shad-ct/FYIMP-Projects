d64e4373fb0d3170eac32a82e793a47ee1862b83
Shad CT <88387228+shad-ct@users.noreply.github.com>
2024-08-27 19:32:19 +0530

    Update StringPalindrome.c

--- CProgramming/StringPalindrome.c
+++ CProgramming/StringPalindrome.c
@@ -5,7 +5,7 @@
 int main(){
 	char name[100] = "malayalam";
 	int i , j,length=0,flag = 1;
-	while(name[length]!='\0'){ // process of strlen()
+	while(name[length]!='\0'){ // process of strlen() , you can also do strlen(name);
 		length++;
 	}
 	for(i=0,j=length-1; i<j ; i++,j--){
