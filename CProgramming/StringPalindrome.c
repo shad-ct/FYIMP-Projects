f2cac4d3794091ba526eecf1ff4927da1a946b74
Shad CT <88387228+shad-ct@users.noreply.github.com>
2024-08-27 19:30:49 +0530

    Update StringPalindrome.c

--- CProgramming/StringPalindrome.c
+++ CProgramming/StringPalindrome.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-int main(){
-	char string[100],rev[100];
-	scanf("%s",string);
-	strcpy(rev,string);//(to, from)
-	//if(rev == string), dont do this cause it compares the memory addresses. 
-	//becasue they are arrays
-	// instead compine them, it compares the string and returns 0 if they are equal lol 
-	if (strcmp(rev, string) == 0){
-		printf("its a pal");
-	}
-	else{
-		printf("its not a pal");
-	}
-	
-}
 
-// ANOTHER METHOD
 
-/*
 #include <stdio.h>
 #include <string.h>
 int main(){
@@ -40,4 +21,27 @@ int main(){
 		printf("not");
 	}
 }
+
+// ANOTHER METHOD
+
+/*
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char string[100],rev[100];
+	scanf("%s",string);
+	strcpy(rev,string);//(to, from)
+ 	strrev(rev);
+	//if(rev == string), dont do this cause it compares the memory addresses. 
+	//becasue they are arrays
+	// instead compine them, it compares the string and returns 0 if they are equal lol 
+	if (strcmp(rev, string) == 0){
+		printf("its a pal");
+	}
+	else{
+		printf("its not a pal");
+	}
+	
+}
+
 */
