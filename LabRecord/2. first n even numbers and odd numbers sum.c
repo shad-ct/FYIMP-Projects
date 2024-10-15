cd3dc5db6f439649ec2f6f27efd866947c6370a3
Shad CT <88387228+shad-ct@users.noreply.github.com>
2024-10-15 06:29:15 +0530

    Create 2. first n even numbers and odd numbers sum.c

---
+++ LabRecord/2. first n even numbers and odd numbers sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // first n even numbers and odd numbres sum
+    
+    int n,i,even=0,odd =0;
+    printf("enter the n : ");
+    scanf("%d",&n);
+    
+    for(i=0;i<=n;i++){
+        if(i%2==1){
+            odd = odd + i;
+        }
+        
+        if(i%2==0){
+            even = even + i;
+        }
+    }
+    printf("sum of %d odd numbers = %d \n",n,odd);
+    printf("sum of %d odd numbers = %d",n,even);
+}
